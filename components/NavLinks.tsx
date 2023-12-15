d5c14c881caa9e73ebd0038f5b879330dd5a8c6d
Berl Treasure Campomanes <berlcamp@gmail.com>
2023-12-15 16:48:30 +0800

    remove search voter

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -10,7 +10,7 @@ export const NavLinks = () => {
       {/* <Link className={`inline-flex ${currentRoute === '/' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`} href='/'>Duplicates</Link>
       <Link className={`inline-flex ${currentRoute === '/notregistered' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/notregistered'>Not&nbsp;Registered</Link>*/}
       <Link className={`inline-flex ${currentRoute === '/households' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/households'>Households</Link>
-      <Link className={`inline-flex ${currentRoute === '/search' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/search'>Registered</Link>
+      {/* <Link className={`inline-flex ${currentRoute === '/search' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/search'>Registered</Link> */}
       <Link className={`inline-flex ${currentRoute === '/heads' ? 'underline underline-offset-4 font-bold text-gray-200' : 'text-gray-300'}`}  href='/heads'>Head&nbsp;SP</Link>
     </div>
   )
